6d6069fa13207800a57cb41664ba1e363853a494
lllll-128 <leytelx@126.com>
2024-07-16 01:08:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,96 +1,19 @@
-/* eslint-disable */
-
-const USER_CONFIG = {
-  /**
-   * 基本配置
-   */
-  
- 
-  USE_PASSAGE: 'wechat-test',
-  
-  
-  APP_ID: 'wxe599ddfca42cb074',
-
-  
-  APP_SECRET: '6c8425138705ba7a7dfcbef9475f8206',
-  
-  
-  MAX_PUSH_ONE_MINUTE: 5,
-  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
-  SLEEP_TIME: 65000,
-
-  // 功能开关,打开：true，关闭：false
-  SWITCH: {
-    /** 每日天气 */
-    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    weather: true,
-
-    /** 节假日 */
-    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    holidaytts: true,
-
-    
-    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    earthyLoveWords: true,
-    
-  
-    /** 生日消息和节日消息 */
-    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    birthdayMessage: true,
-  
-   
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
-   */
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: 'Yue宝宝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'ov2176JF0lE1p5RGScYM0F6XpCxw',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'NxWK0pLVxtzhi7ujQBGmBPlbuwHgd7Gxp6-F8H6tjKA',
-      // 所在省份或城市，也可以不填
-      province: '辽宁',
-      // 所在城市或县区
-      city: '大连',
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '09-11',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: 'Yue宝宝', year: '1999', date: '09-11',
-        },
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2021-08-30' },
-      ],
-     
- 
-  /** 插槽 */
-
-  /** 你可以在这里写超多的你想显示的内容了！
-   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
-   * */
-  SLOT_LIST: [
-    // 这样配置的话，就会每次随机选一句话发送
-    {
-      keyword: 'lover_prattle',
-      contents: [
-        '因为我爱你，自此有了撞南墙的勇气。',
-        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
-        '遇见你，我真的不想再放弃。'
-      ],
-    }
-}
-module.exports = USER_CONFIG
-
+{"APP_ID":"wxe599ddfca42cb074","APP_SECRET":" 6c8425138705ba7a7dfcbef9475f8206",
+"IS_SHOW_COLOR":true,
+"USERS":[{"name":"Yue宝宝","id":"ov2176JF0lE1p5RGScYM0F6XpCxw",
+"useTemplateId":"W42z6oKZe6FbEZMz9baSY2YBdX7SGaXqmTRQ4-kB_6Y",
+"province":"辽宁","city":"大连",
+"horoscopeDate":"09-11",
+"horoscopeDateType":"今日",
+"festivals":[{"type":"生日","name":"Yue宝宝的生日","date":"09-11","year":"1999"}],
+"customizedDateList":[{"keyword":"我们在一起恋爱",
+"date":"2021-08-29"}]}],"SWITCH":
+{"weather":true,"holidaytts":false,
+"CIBA":true,
+"oneTalk":false,
+"earthyLoveWords":true,"
+momentCopyrighting":false,"
+poisonChickenSoup":false,
+"poetry":false,
+"horoscope":true,
+"birthdayMessage":true}}
