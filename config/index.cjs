18f5918252ed8b0bdc81088b129d51b7a9695d47
lllll-128 <leytelx@126.com>
2024-07-15 22:06:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,41 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '6c8425138705ba7a7dfcbef9475f8206',
 
-  PROVINCE: '辽宁',
-  CITY: '大连',
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: true,
+    
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: true,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: true,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  
+  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  IS_SHOW_COLOR: true,
+  
+  /** 每日一言 */
+  // 每日一言的内容类型
+  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+  LITERARY_PREFERENCE: '',
 
   USERS: [
     {
@@ -24,6 +57,10 @@ const USER_CONFIG = {
       id: 'ov2176JF0lE1p5RGScYM0F6XpCxw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'NxWK0pLVxtzhi7ujQBGmBPlbuwHgd7Gxp6-F8H6tjKA',
+      // 所在省份或城市，也可以不填
+      province: '辽宁',
+      // 所在城市或县区
+      city: '大连',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-11',
       festivals: [
@@ -46,6 +83,14 @@ const USER_CONFIG = {
       ],
     },
   ],
+   {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为我爱你，所以拿出撞南墙的勇气。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '这一次，我真的不想放弃。'
+      ],
+    }
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
