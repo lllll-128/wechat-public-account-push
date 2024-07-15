03050ac6972e3823b191ebc7a615b66d23db59da
lllll-128 <leytelx@126.com>
2024-07-15 22:32:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,18 +1,35 @@
 /* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
+  /**
+   * 基本配置
+   */
+  
+  // 使用的推送通道：['push-deer', 'wechat-test', 'server-chan', 'push-plus']
+  // 默认使用 【微信测试号】
+  // 使用【pushDeer】请填写 push-deer
+  // 使用【微信测试号】请填写 wechat-test
+  // 使用【方糖服务号】请填写 server-chan
+  // 使用【pushplus推送加服务号】请填写 push-plus
+  USE_PASSAGE: 'wechat-test',
+  
+  // 使用微信测试号时才需要填写：公众号APP_ID
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
   APP_ID: 'wxe599ddfca42cb074',
 
-  // 使用微信测试号：公众号APP_SECRET
+  // 使用微信测试号时才需要填写：公众号APP_SECRET
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: '6c8425138705ba7a7dfcbef9475f8206',
+  
+  // 为了避免推送服务器误将脚本列为恶意推送脚本，可设置每分钟脚本最大推送数
+  // 每分钟脚本最大推送数，超过此数将会休眠1分钟后再发送剩余消息，不填则默认为5
+  // 此项不建议随意修改
+  MAX_PUSH_ONE_MINUTE: 5,
+  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
+  SLEEP_TIME: 65000,
 
+  // 功能开关,打开：true，关闭：false
   SWITCH: {
     /** 每日天气 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
@@ -24,13 +41,22 @@ const USER_CONFIG = {
 
     /** 每日N句 */
     // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    CIBA: true,
+    CIBA: false,
+    
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: false,
     
     // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     earthyLoveWords: true,
     
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+    
     // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    poisonChickenSoup: true,
+    poisonChickenSoup: false,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
 
     /** 星座运势 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
@@ -40,6 +66,32 @@ const USER_CONFIG = {
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     birthdayMessage: true,
   
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
+  
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: false,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 0,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 0,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'title',
+  },
+  
   /** 是否给文字设置多彩颜色, 和emoji不兼容 */
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
   IS_SHOW_COLOR: true,
@@ -48,62 +100,65 @@ const USER_CONFIG = {
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
   LITERARY_PREFERENCE: '',
+  
 
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+   */
   USERS: [
     {
       // 想要发送的人的名字
-      name: '越越宝宝',
+      name: 'Yue宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'ov2176JF0lE1p5RGScYM0F6XpCxw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: 'NxWK0pLVxtzhi7ujQBGmBPlbuwHgd7Gxp6-F8H6tjKA',
       // 所在省份或城市，也可以不填
       province: '辽宁',
       // 所在城市或县区
       city: '大连',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '09-11',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: 'Yue', year: '1999', date: '08-02',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: 'Yue', year: '1999', date: '09-11',
+          type: '生日', name: 'Yue宝宝', year: '1999', date: '09-11',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2021', date: '08-21',
-        },
+          type: '生日', name: 'Lin', year: '2003', date: '12-18',
+        }
       ],
-      // 我们在一起已经有xxxx天了的配置
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2021-08-31' },
+        { keyword: 'love_day', date: '2021-08-30' },
       ],
-    },
-  ],
-   {
+     
+ 
+  /** 插槽 */
+
+  /** 你可以在这里写超多的你想显示的内容了！
+   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
+   * */
+  SLOT_LIST: [
+    // 这样配置的话，就会每次随机选一句话发送
+    {
       keyword: 'lover_prattle',
       contents: [
-        '因为我爱你，所以拿出撞南墙的勇气。',
+        '因为我爱你，自此有了撞南墙的勇气。',
         '你很傻，你很笨，可我还是很羡慕你，因为你有我',
-        '这一次，我真的不想放弃。'
+        '遇见你，我真的不想再放弃。'
       ],
     }
+  
 
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
+module.exports = USER_CONFIG
 }
 
 module.exports = USER_CONFIG
